621516c56c761462b3e92e73511a86685407f2e2
LeaPhant <mail@lea.moe>
2026-06-22 16:39:23 +0200

    portal: disable gifv video forward It should be handled by the client and is only displayed correctly in Beeper beyond forks

--- portal_convert.go
+++ portal_convert.go
@@ -688,10 +688,10 @@ func isPlainGifMessage(msg *discordgo.Message) bool {
 		return false
 	}
 	embed := msg.Embeds[0]
-	isGifVideo := embed.Type == discordgo.EmbedTypeGifv && embed.Video != nil
+	// isGifVideo := embed.Type == discordgo.EmbedTypeGifv && embed.Video != nil
 	isGifImage := embed.Type == discordgo.EmbedTypeImage && embed.Image == nil && embed.Thumbnail != nil && embed.Title == ""
 	contentIsOnlyURL := msg.Content == embed.URL || discordLinkRegexFull.MatchString(msg.Content)
-	return contentIsOnlyURL && (isGifVideo || isGifImage)
+	return contentIsOnlyURL && isGifImage
 }
 
 func (portal *Portal) convertDiscordMentions(msg *discordgo.Message, syncGhosts bool) *event.Mentions {
